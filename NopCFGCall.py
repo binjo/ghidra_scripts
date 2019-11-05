0def87f35e46bde6f0ce20ebb1d1efa9235a3d91
binjo <binjo.cn@gmail.com>
2019-11-05 16:43:12 +0800

    NopCFGCall.py
    refactored, patchProgram rocks...

--- NopCFGCall.py
+++ NopCFGCall.py
@@ -8,8 +8,10 @@
 from ghidra.app.plugin.assembler import Assemblers
 from ghidra.program.database.code import InstructionDB
 from ghidra.program.model.address import GenericAddress
+from ghidra.app.util import XReferenceUtil
 
 asm = Assemblers.getAssembler(currentProgram)
+xref = XReferenceUtil()
 
 def patch_many_bytes(frm, opcode="NOP"):
     addr, size = None, 0
@@ -22,103 +24,136 @@ def patch_many_bytes(frm, opcode="NOP"):
     else:
         print "[-] wrong param...{0}".format(type(frm))
         return
-   
-    for i in xrange(size):
-        asm.assemble(addr.add(i), opcode)
 
-def find_inst_via_pattern(frm_inst, pattern1=None, pattern2=None, tot=50, fwd_p=True):
-    ctn, the_inst, the_addr = 0, None, frm_inst.next.getAddress()
-    while True:
-        if ctn > tot:
-            print "[-] too far away...{0}, {1}".format(tot, the_addr)
-            break
+    # assemble call is way tooo slow...
+    # for i in xrange(size):
+    #     asm.assemble(addr.add(i), opcode)
+    asm.patchProgram("\x90"*size, addr)
 
-        inst = getInstructionAt(the_addr)
-        if not inst:
-            print "[-] failed to get inst...{0}".format(the_addr)
-            break
+class CFGXrefs(object):
+    """
+    """
+    def __init__(self, icall=None):
+        self._icall = icall     # why keep it :X
+        self._xrefs = []
 
-        tsni = str(inst).lower()
-        if pattern1:
-            if tsni.startswith(pattern1):
-                if pattern2:
-                    if tsni.endswith(pattern2):
+    def exists(self, addr):
+        return addr in self._xrefs
+
+    @property
+    def xrefs(self):
+        return self._xrefs
+
+    def append(self, addr):
+        if addr and not self.exists(addr):
+            self._xrefs.append(addr)
+
+    def find(self, frm_inst, pattern1=None, pattern2=None, tot=50, fwd_p=True):
+        ctn, the_inst, the_addr = 0, None, None
+        if fwd_p:
+            the_addr = frm_inst.next.getAddress()
+        else:
+            # backward finding, check if has reference
+            if xref.getReferenceCount(frm_inst) == 1:
+                the_addr = xref.getXRefList(frm_inst)[0] # FIXME other index possible?
+            else:
+                the_addr = frm_inst.previous.getAddress()
+
+        while True:
+            if ctn > tot:
+                break
+
+            inst = getInstructionAt(the_addr)
+            if not inst:
+                print "[-] failed to get inst...{0} {1}".format(the_addr, frm_inst.getAddress())
+                break
+
+            tsni = str(inst).lower()
+            if pattern1:
+                if tsni.startswith(pattern1):
+                    if pattern2:
+                        if tsni.endswith(pattern2):
+                            the_inst = inst
+                            break
+                    else:
                         the_inst = inst
                         break
+
+            if fwd_p:
+                ft = inst.getFlowType()
+                if ft.isJump():
+                    the_addr = inst.getAddress(0)
                 else:
-                    the_inst = inst
-                    break
-
-        if fwd_p:
-            ft = inst.getFlowType()
-            if ft.isJump():
-                the_addr = inst.getAddress(0)
+                    the_addr = inst.next.getAddress()
             else:
-                the_addr = inst.next.getAddress()
-        else:
-            the_addr = inst.previous.getAddress()
-        ctn += 1
+                if xref.getReferenceCount(inst) == 1:
+                    the_addr = xref.getXRefList(inst)[0]
+                else:
+                    the_addr = inst.previous.getAddress()
+            ctn += 1
 
-    return the_inst
+        if the_inst:
+            self.append(the_inst)
 
-# find xrefs of ___guard_check_icall_fptr call, make selection
-# or just place cursor at the call
-def patch_cfg_call_at(addr):
+        return the_inst
 
-    nop_them_all_insts = []
+    # find xrefs of ___guard_check_icall_fptr call, make selection
+    # or just place cursor at the call
+    def collect_xrefs(self, addr):
+        self.append(addr)
 
-    mov_ecx_inst = find_inst_via_pattern(getInstructionAt(addr), pattern1="mov ecx,", fwd_p=False)
-    if mov_ecx_inst:
+        mov_ecx_inst = self.find(getInstructionAt(addr), pattern1="mov ecx,", fwd_p=False)
+        if not mov_ecx_inst:
+            print "[-] failed to find ecx setting opcodes ... {0}".format(addr)
 
-        nop_them_all_insts.append(mov_ecx_inst)
+        mov_esp_inst = self.find(getInstructionAt(addr), "mov ", ",esp", fwd_p=False)
+        if not mov_esp_inst:
+            print "[-] failed to find esp setting opcodes ... {0}".format(addr)
 
-        mov_esp_inst = find_inst_via_pattern(mov_ecx_inst, "mov ", ",esp", fwd_p=False)
-        if mov_esp_inst:
-            nop_them_all_insts.append(mov_esp_inst)
-        else:
-            print "[-] failed to find esp setting opcodes ... {0}".format(mov_ecx_inst.getAddress())
+        # following instructions could be patched due to compiler optimizations...
+        cmp_esp_inst = self.find(getInstructionAt(addr), "cmp ", ",esp")
+        if cmp_esp_inst:
+            j_inst = cmp_esp_inst.next
+            if str(j_inst).startswith("JNZ"):
+                self.append(j_inst)
+                j_inst = getInstructionAt(j_inst.getAddress(0)).previous # find_inst_via_pattern needs to be previous inst
+            elif str(j_inst).startswith("JZ"):
+                self.append(j_inst)
+            elif str(j_inst).startswith("JMP"):
+                j_inst = getInstructionAt(j_inst.getAddress(0))
+                if str(j_inst).startswith("JNZ"):
+                    self.append(j_inst)
+                    j_inst = getInstructionAt(j_inst.getAddress(0)).previous
+                elif str(j_inst).startswith("JZ"):
+                    self.append(j_inst)
+                else:
+                    print "[-] still not jz/jnz ... {0}".format(j_inst.getAddress())
+            else:
+                print "[-] not jz/jnz ... {0}".format(j_inst.getAddress())
 
-    # following instructions could be patched due to compiler optimizations...
-    cmp_esp_inst = find_inst_via_pattern(getInstructionAt(addr), "cmp ", ",esp")
-    if cmp_esp_inst:
+            mov_ecx_4_inst = self.find(j_inst, pattern1="mov ecx,0x4")
+            if not mov_ecx_4_inst:
+                print "[-] can't find mov ecx, 0x4 ... maybe optimized {0}".format(j_inst.getAddress())
 
-        nop_them_all_insts.append(cmp_esp_inst)
+            int_29_inst = self.find(j_inst, pattern1="int 29")
+            if not int_29_inst:
+                print "[-] can't find int 29 ... maybe optimized {0}".format(j_inst.getAddress())
 
-        j_inst = cmp_esp_inst.next
-        if str(j_inst).startswith("JNZ"):
-            nop_them_all_insts.append(j_inst)
-            j_inst = getInstructionAt(j_inst.getAddress(0)).previous # find_inst_via_pattern needs to be previous inst
-        elif str(j_inst).startswith("JZ"):
-            nop_them_all_insts.append(j_inst)
-        else:
-            print "[-] not jz/jnz ... {0}".format(j_inst.getAddress())
 
-        mov_ecx_4_inst = find_inst_via_pattern(j_inst, pattern1="mov ecx,0x4")
-        if not mov_ecx_4_inst:
-            print "[-] can't find mov ecx, 0x4 ... maybe optimized {0}".format(j_inst.getAddress())
-        else:
-            nop_them_all_insts.append(mov_ecx_4_inst)
-
-        int_29_inst = find_inst_via_pattern(j_inst, pattern1="int 29")
-        if not int_29_inst:
-            print "[-] can't find int 29 ... maybe optimized {0}".format(j_inst.getAddress())
-        else:
-            nop_them_all_insts.append(int_29_inst)
-
-    # should be safe to patch now...
-    patch_many_bytes(addr)
-    for inst in nop_them_all_insts:
-        patch_many_bytes(inst)
-
+x = CFGXrefs()
 
 if currentSelection:
     ctn = 0
     for addr in iter(currentSelection):
-        patch_cfg_call_at(addr.getMinAddress())
+        x.collect_xrefs(addr.getMinAddress())
         ctn += 1
         if (ctn % 100 == 0):
             print "[+] done...{0}".format(ctn)
     print "[+] total...{0}".format(ctn)
 else:
-    patch_cfg_call_at(currentAddress)
-    print "[+] check at current address..."
+    x.collect_xrefs(currentAddress)
+
+for xref in x.xrefs:
+    patch_many_bytes(xref)
+
+print "[+] done ... {0} blocks".format(len(x.xrefs))
